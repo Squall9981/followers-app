a4c59f35fd819a9dcae8b37bfe988f1f38448a54
Squall9981 <Squall9981@hotmail.com>
2018-06-07 15:32:14 -0400

    Prepare for heroku

--- CompleteAngularCourse/Deployment/deploy-demo/server.js
+++ CompleteAngularCourse/Deployment/deploy-demo/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+//Host static content from dist folder, where build is stored
 app.use(express.static(__dirname + '/dist'));
 
+// .... add API endpoints here
+
+//Catch all invalid routes, send index.html to client if invalid route.
 app.all('*', (req, res) => {
     res.status(200).sendFile(__dirname = '/dist/index.html');
 });
 
+//Listen on environment port
 app.listen(process.env.PORT || 8080);
